10c1face52a0ff55a3410dff66c28b8d99ebb0dc
Lucas-prog-ctrl <69973688+Lucas-prog-ctrl@users.noreply.github.com>
2020-09-30 16:46:48 -0300

    Create Exemplo1.py

---
+++ LTP2/Pratica2/Exemplo1.py
@@ -0,0 +1,13 @@
+#Determinar se um número inteiro é par ou Impar
+numero = int (input ("Numero:"))
+#Calcula o resto da divisão de numero por 2
+resto = numero% 2
+
+# Condição
+se resto == 0:
+  imprimir ("PAR")
+  imprimir ("Vnecendor")
+  print ("Não desista de seus objetivos")
+  print ("O mindset é chave do sucesso!")
+
+print ("Esse sempre aparece!")
