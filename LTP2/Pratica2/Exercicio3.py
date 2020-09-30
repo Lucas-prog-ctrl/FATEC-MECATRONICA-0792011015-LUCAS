778df4c8e08861b585e543f08c8e16daa075d858
Lucas-prog-ctrl <69973688+Lucas-prog-ctrl@users.noreply.github.com>
2020-09-30 16:49:40 -0300

    Create Exercicio3.py

---
+++ LTP2/Pratica2/Exercicio3.py
@@ -0,0 +1,16 @@
+#Determinar se um número inteiro é par ou Impar
+numero = int(input("Numero:"))
+#Calcula o resto da divisão de numero por 2
+resto = numero % 2
+
+#Condição
+if resto == 0:
+  print("PAR")
+  print("Vnecendor")
+  print("Não desista de seus objetivos")
+  print("O mindset é chave do sucesso!")
+else:
+  print("Impar")
+  print("Nver Give Up")
+  print("Never desista dos seus dreams")
+print("Esse sempre aparece!")
