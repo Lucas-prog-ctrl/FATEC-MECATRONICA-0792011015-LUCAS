40c4e3334eb3540ca26382391de0c1805c051bd5
Lucas-prog-ctrl <69973688+Lucas-prog-ctrl@users.noreply.github.com>
2020-10-15 09:58:48 -0300

    Create Exemplo1.py

---
+++ LTP2/Pratica08/Exemplo1.py
@@ -0,0 +1,10 @@
+#Cria um dicionario vazio
+dicionario = {}
+
+#Adiciona um valor ao dicionario
+dicionario["eletrico"] = 'pikachu'
+dicionario["fogo"] = 'chariz'
+dicionario["agua"] = "blastoise"
+dicionario[1] = "Lalala"
+
+print(dicionario)
